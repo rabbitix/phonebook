197328efc5daf57da481e4a16d22b3d9eecb28f9
Ali <rabbitix98@gmail.com>
2023-08-19 23:30:44 +0330

    add contact and number models

---
+++ phonebook/models.py
@@ -0,0 +1,24 @@
+import datetime
+from sqlalchemy import Boolean, Column, ForeignKey, String, Integer, DateTime
+from sqlalchemy.orm import relationship, backref
+from .database import Base
+
+
+class Contact(Base):
+    __tablename__ = 'contacts'
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String, )
+    last_name = Column(String, )
+    nick_name = Column(String, nullable=True)
+    created = Column(DateTime, default=datetime.datetime.now)
+
+
+class Number(Base):
+    __tablename__ = 'numbers'
+    id = Column(Integer, primary_key=True, index=True)
+    label = Column(String, default="phone")
+    phone = Column(String, unique=True)
+    is_default = Column(Boolean, default=False)
+    contact_id = Column(Integer, ForeignKey('contacts.id'))
+    # set lazy='dynamic' to be abel to query on relationship
+    contact = relationship("Contact", backref=backref('numbers', lazy='dynamic'))
