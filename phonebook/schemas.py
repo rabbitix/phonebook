97ff8ff793307b8eb9a739faed912ec083e0da5a
Ali <rabbitix98@gmail.com>
2023-08-25 23:51:31 +0330

    update schemas: add default value for created field also make optional things optional

--- phonebook/schemas.py
+++ phonebook/schemas.py
@@ -1,17 +1,18 @@
 import datetime
 from pydantic import BaseModel, Field
+from typing import Optional
 
 
 class NumberCreate(BaseModel):
-    label: str = "phone"
+    label: Optional[str] = "phone"
     phone: str = Field(pattern=r"^09(1[0-9]|3[1-9]|2[1-9])?[0-9]{3}?[0-9]{4}$")
-    is_default: bool = False
+    is_default: Optional[bool] = False
 
 
 class NumberPartialUpdate(BaseModel):
-    label: str = None
-    phone: str = None
-    is_default: bool = None
+    label: Optional[str] = None
+    phone: Optional[str] = None
+    is_default: Optional[bool] = None
 
 
 class Number(NumberCreate):
@@ -25,8 +26,8 @@ class Number(NumberCreate):
 class BaseContact(BaseModel):
     first_name: str
     last_name: str
-    nick_name: str = None
-    created: datetime.datetime = None
+    nick_name: Optional[str] = None
+    created: Optional[datetime.datetime] = Field(default=datetime.datetime.now(), )
 
 
 class ContactCreate(BaseContact):
@@ -34,9 +35,9 @@ class ContactCreate(BaseContact):
 
 
 class ContactPartialUpdate(BaseModel):
-    first_name: str = None
-    last_name: str = None
-    nick_name: str = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    nick_name: Optional[str] = None
 
 
 class FullContact(BaseContact):
